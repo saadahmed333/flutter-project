5fab68aef93bdc90a5d41df5a3497091584cf4d0
ibtihajkhan <saadahmed1742@gmail.com>
2023-10-21 06:48:39 -0700

    get input values and route to other screen

--- lib/loginview.dart
+++ lib/loginview.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:meriapp/signupview.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
+
+  onLoginPressed(BuildContext context, emailController, passwordController) {
+    String email = emailController.text;
+    if (email.contains("@") && email.contains(".com")) {
+      print("Email: ${emailController.text}");
+      print("password: ${passwordController.text}");
+    } else {
+      print("In Correct Email");
+    }
+    emailController.text = "";
+    passwordController.text = "";
+    Navigator.push(context,
+        MaterialPageRoute(builder: (BuildContext context) => signupView()));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,18 +61,38 @@ class LoginView extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            const SizedBox(
-                width: double.infinity,
-                height: 20,
-                child: Text(
-                  "Forget Password",
-                  textAlign: TextAlign.right,
-                )),
+            InkWell(
+              onTap: () {
+                print("Hello world");
+              },
+              child: const SizedBox(
+                  width: double.infinity,
+                  height: 20,
+                  child: Text(
+                    "Forget Password",
+                    textAlign: TextAlign.right,
+                  )),
+            ),
             SizedBox(
                 height: 40,
                 width: double.infinity,
                 child: ElevatedButton(
-                    onPressed: () {}, child: const Text("Login")))
+                    onPressed: () {
+                      onLoginPressed(
+                          context, emailController, passwordController);
+                    },
+                    child: const Text("Login"))),
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => signupView()));
+                },
+                child: const Text("Dont have an account, SignUp"))
           ],
         ),
       ),
